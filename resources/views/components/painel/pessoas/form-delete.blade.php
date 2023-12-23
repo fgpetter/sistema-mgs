bbb122e871fb6ee47528f8ff32c454e157584835
FILIPE PETTER <fgpetter@gmail.com>
2023-12-23 12:52:07 -0300

    remove rotas e views desnecessarias

--- resources/views/components/painel/pessoas/form-delete.blade.php
+++
@@ -1,5 +0,0 @@
-<form class="d-flex justify-content-end pb-3" style="margin-top: -2rem" method="POST" 
-  action="{{ route($route, $id) }}">
-  @csrf
-  <button type="submit" class="btn btn-sm btn-danger">Remover pessoa</button>
-</form>
--- resources/views/components/painel/post/form-delete.blade.php
+++
@@ -1,4 +0,0 @@
-<form class="d-flex justify-content-end pb-3" style="margin-top: -2rem" method="POST" action="{{ route($route, $id) }}">
-    @csrf
-    <button type="submit" class="btn btn-sm btn-danger">Remover</button>
-</form>
