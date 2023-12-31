5e44a5c643c45aa6374c5c4347e39640e09c1438
FILIPE PETTER <fgpetter@gmail.com>
2023-12-31 14:56:53 -0300

    ajustes pequenos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,16 @@
 @extends('layouts.master')
-@section('title') @lang('translation.starter') @endsection
+@section('title')
+    @lang('translation.starter')
+@endsection
 @section('content')
-@component('components.breadcrumb')
-@slot('li_1') Inicio @endslot
-@slot('title') Painel @endslot
-@endcomponent
+    @component('components.breadcrumb')
+        @slot('li_1')
+            Inicio
+        @endslot
+        @slot('title')
+            Painel
+        @endslot
+    @endcomponent
 @endsection
 @section('script')
 @endsection
