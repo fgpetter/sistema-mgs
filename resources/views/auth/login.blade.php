a40a0721d9e6d3826d588a8695e7f2e7904180e9
FILIPE PETTER <fgpetter@gmail.com>
2024-01-25 18:45:33 -0300

    pequenos ajustes de frontend

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 <head>
 
   <meta charset="utf-8" />
-  <title> Login | Rede Metrológica RS</title>
+  <title> Login | MGS</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
